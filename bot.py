b2369ca6b0596d83c9285b065a49f2bb902cb005
lenargasimov <lenargasimov@duck.com>
2022-04-14 19:22:40 +0300

    test build

--- bot.py
+++ bot.py
@@ -47,45 +47,45 @@ class TimeSchedule():
             print('Сегодня нет именинников.')
             
 
-mas = []
-if os.path.exists('data/dialog.txt'):
-    f = open('data/dialog.txt', 'r', encoding='UTF-8')
-    for x in f:
-        if (len(x.strip()) > 2):
-            mas.append(x.strip().lower())
-    f.close()
-
-
-def answer(text):
-    try:
-        text = text.lower().strip()
-        if os.path.exists('data/dialog.txt'):
-            a = 0
-            n = 0
-            nn = 0
-            for q in mas:
-                if ('Q=: ' in q):
-                    aa = (fuzz.token_sort_ratio(q.replace('Q: ', ''), text))
-                    if (aa > a and aa!= a):
-                        a = aa
-                        nn = n
-                n = n + 1
-            s = mas[nn + 1]
-            return s
-        else:
-            return 'Ошибка'
-    except:
-        return 'Ошибка'
-
-
-@bot.message_handler(content_types=["text"])
-def handle_text(message):
-    time.sleep(10)
-    f = open('data/' + str(message.chat.id) + '_log.txt', 'a', encoding='UTF-8')
-    s = answer(message.text)
-    f.write('Q: ' + message.text + '\n' + s + '\n')
-    f.close()
-    bot.send_message(message.chat.id, s.capitalize())
+# mas = []
+# if os.path.exists('data/dialog.txt'):
+#     f = open('data/dialog.txt', 'r', encoding='UTF-8')
+#     for x in f:
+#         if (len(x.strip()) > 2):
+#             mas.append(x.strip().lower())
+#     f.close()
+#
+#
+# def answer(text):
+#     try:
+#         text = text.lower().strip()
+#         if os.path.exists('data/dialog.txt'):
+#             a = 0
+#             n = 0
+#             nn = 0
+#             for q in mas:
+#                 if ('Q=: ' in q):
+#                     aa = (fuzz.token_sort_ratio(q.replace('Q: ', ''), text))
+#                     if (aa > a and aa!= a):
+#                         a = aa
+#                         nn = n
+#                 n = n + 1
+#             s = mas[nn + 1]
+#             return s
+#         else:
+#             return 'Ошибка'
+#     except:
+#         return 'Ошибка'
+#
+#
+# @bot.message_handler(content_types=["text"])
+# def handle_text(message):
+#     time.sleep(10)
+#     f = open('data/' + str(message.chat.id) + '_log.txt', 'a', encoding='UTF-8')
+#     s = answer(message.text)
+#     f.write('Q: ' + message.text + '\n' + s + '\n')
+#     f.close()
+#     bot.send_message(message.chat.id, s.capitalize())
 
 
 @bot.message_handler(regexp='Денис когда выйдет новый эфир Технополис?')
